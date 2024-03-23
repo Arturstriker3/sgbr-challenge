13762e294dc2ac080b8b1a85aa2544cf52e21c90
Arturstriker3 <artur.danielgama@gmail.com>
2024-03-23 02:07:12 -0300

    "Feat": Grid with gifs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,32 +1,60 @@
 <template>
 
-  
-
   <div class="search-container">
     <div class="search">
       <div class="icon" @click="activateSearch"></div>
       <div class="input">
-        <input type="text" placeholder="Search your GIPHY..." id="InputSearch">
+        <input type="text" placeholder="Search your GIPHY..." id="InputSearch" @keyup.enter="searchContent">
         <span class="delete" @click="activateSearch"></span>
       </div>
     </div>
   </div>
+
+  <div class="content" v-show="showContent">
+    <div class="gif-grid">
+      <div v-for="gif in gifs" :key="gif.id" class="gif-item">
+        <a href="#">
+          <img :src="gif.images.fixed_height.url" alt="GIF">
+        </a>
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <script>
+
+import axios from 'axios';
+
 export default {
   methods: {
     activateSearch() {
-      this.SearchBarActive = !this.SearchBarActive;
+      this.searchBarOn = !this.SearchBarActive;
       const searchElement = document.querySelector('.search');
       searchElement.classList.toggle('active');
       const containerElement = document.querySelector('.search-container');
-      containerElement.classList.toggle('search-active'); // Adiciona ou remove a classe search-active
-    }
+      containerElement.classList.toggle('search-active');
+      this.showContent = false;
+    },
+
+    async searchContent() {
+      try {
+        const apiKey = 'PbfMrZT4cuSTz5QVE5bPguk1OKkXYh5s';
+        const query = document.getElementById('InputSearch').value;
+        const response = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}`);
+        
+        this.gifs = response.data.data;
+        this.showContent = true;
+      } catch (error) {
+        console.error('Error fetching GIFs:', error);
+      }
+    },
   },
   data() {
     return {
-      SearchBarActive: false,
+      searchBarOn: false,
+      showContent: false,
+      gifs: []
     };
   }
 };
@@ -36,9 +64,6 @@ export default {
 
 @import '../assets/_main.scss';
 
-
-
-
 // SearchBar
 
 .search-container {
@@ -47,7 +72,7 @@ export default {
   justify-content: center;
   height: 100vh;
   background: $colorBlack;
-  transition: height 0.5s ease; /* Add transition property */ 
+  transition: height 0.5s ease; 
 
   &.search-active {
     height: calc(100vh - 80vh);
@@ -151,6 +176,38 @@ export default {
           background: $colorBlack;
           transform: rotate(315deg);
         }
+      }
+    }
+  }
+}
+
+// Gif Content
+
+.content {
+
+  margin: 16px 32px;
+
+  .gif-grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    gap: 16px;
+    transition: max-height 0.3s ease;
+
+    .gif-item {
+      overflow: hidden;
+      border-radius: 8px;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+      transition: transform 0.2s ease, filter 0.5s ease;
+
+      img {
+        width: 100%;
+        height: auto;
+        object-fit: cover;
+      }
+
+      &:hover {
+        transform: scale(1.05);
+        filter: brightness(50%);
       }
     }
   }
